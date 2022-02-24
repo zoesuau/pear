601dfc64d266af99315a9e104a4577e469c2f568
zoe <zoesuau@gmail.com>
2022-02-24 22:38:53 +0800

    new

---
+++ public/js/product.js
@@ -0,0 +1,40 @@
+$("#thumbnails>img").on("click", function () {
+
+
+    $("#main>img").attr("src", $(this).attr("src"))
+
+    $(this).siblings().removeClass("active1")
+
+})
+$("#thumbnails2>img").on("click", function () {
+
+
+    $("#main2>img").attr("src", $(this).attr("src"))
+
+    $(this).siblings().removeClass("active1")
+
+})
+$("#thumbnails3>img").on("click", function () {
+
+
+    $("#main3>img").attr("src", $(this).attr("src"))
+
+    $(this).siblings().removeClass("active1")
+
+})
+$("#thumbnails4>img").on("click", function () {
+
+
+    $("#main4>img").attr("src", $(this).attr("src"))
+
+    $(this).siblings().removeClass("active1")
+
+})
+$("#thumbnails5>img").on("click", function () {
+
+
+    $("#main5>img").attr("src", $(this).attr("src"))
+
+    $(this).siblings().removeClass("active1")
+
+})
